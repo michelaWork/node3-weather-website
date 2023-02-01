a53f4bf206746efcd28b79c4748e9132e8e34278
michelaWork <101396014+michelaWork@users.noreply.github.com>
2023-02-01 10:07:27 +0100

    Setup app for deploying

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //define paths for express config
 const publicDirectory = path.join(__dirname, "../public");
@@ -86,6 +87,6 @@ app.get("*", (req, res) => {
     errorText: "Page not found",
   });
 });
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
